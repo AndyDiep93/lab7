19038c30449b5e680f0aee42677932cc6ad474cf
Bluenurples <andy.diep@edu.sait.ca>
2022-03-27 21:57:07 -0600

    Complete

--- src/main/java/ca/sait/lab7/servlets/UserServlet.java
+++ src/main/java/ca/sait/lab7/servlets/UserServlet.java
@@ -88,6 +88,7 @@ public class UserServlet extends HttpServlet {
             }
         }
 
+        //Edit
         if (action != null && action.equals("editUser")) {
 
             String email = request.getParameter("editEmail");
